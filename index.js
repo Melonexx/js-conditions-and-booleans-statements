8852dd9d00ae08c5e45c0c0c481fcc292375fee9
Eileen Ilsemann <eileenilsemanni@gmail.com>
2023-12-28 23:56:10 +0100

    add solution

--- index.js
+++ index.js
@@ -5,22 +5,44 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD === receivedPassword) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
 // Part 2: Even / Odd
 const number = 6;
+
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
 
+if (numberOfHotdogs < 5 && numberOfHotdogs > 0) {
+  console.log(numberOfHotdogs * 2);
+} else if (numberOfHotdogs >= 5 && numberOfHotdogs < 100) {
+  console.log(numberOfHotdogs * 1.5);
+} else if (numberOfHotdogs >= 100 && numberOfHotdogs < 1000000) {
+  console.log(numberOfHotdogs * 1);
+} else {
+  console.log(numberOfHotdogs * 0.1);
+}
+
 // Part 4: Daytime
 const currentHour = 12;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn..." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
 const name = "Archibald";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (name === "Jessica" ? "Coach" : name) + "!";
 
 console.log(greeting);
